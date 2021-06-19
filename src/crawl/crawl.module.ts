9fee1d884002179463f9256b172182dbd0f65308
Ko-GyeongTae <kokt0203@naver.com>
2021-06-19 18:14:58 +0900

    <EDIT> crawl.module.ts

--- src/crawl/crawl.module.ts
+++ src/crawl/crawl.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CrawlService } from './crawl.service';
 import { CrawlController } from './crawl.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Profile } from 'src/entity/Profile.entity';
+import { Repo } from 'src/entity/Repo.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Profile, Repo])],
   providers: [CrawlService],
   controllers: [CrawlController]
 })
