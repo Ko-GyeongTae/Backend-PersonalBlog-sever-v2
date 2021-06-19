9268bece6e7ca29f79026856aa0a1ac520fcc332
Ko-GyeongTae <kokt0203@naver.com>
2021-06-19 18:11:01 +0900

    <EDIT>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { PostModule } from './post/post.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CrawlModule } from './crawl/crawl.module';
 import Joi from 'joi';
 
 @Module({
-  imports: [AuthModule, PostModule, ConfigModule.forRoot({
+  imports: [CrawlModule, ConfigModule.forRoot({
     envFilePath: process.env.NODE_ENV,
     isGlobal: true,
     /*validationSchema: Joi.object({
@@ -22,19 +20,16 @@ import Joi from 'joi';
     }),*/
   }), 
   TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: 3306,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: 'blog',
-    entities: [
-      __dirname + "/entity/*.{js,ts}"
-    ],
+    type: 'mongodb',
+    url:
+      `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PASSWORD}`,
+    entities: [__dirname + '**/**.entity{.ts,.js}'],
     synchronize: true,
+    useNewUrlParser: true,
+    logging: true,
   }),
 ],
   controllers: [AppController],
-  providers: [AppService, AuthService],
+  providers: [AppService],
 })
 export class AppModule {}
